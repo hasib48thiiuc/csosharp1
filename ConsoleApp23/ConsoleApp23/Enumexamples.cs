00569fef26a23e62bf83f6639d57249bae4089c8
Md.Hasibul Hossain <mdhasibulhossain8@gmail.com>
2022-04-14 01:31:21 +0600

    commiting Enumexamples.cs

---
+++ ConsoleApp23/ConsoleApp23/Enumexamples.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+   public enum paymentstatus
+    {
+        complete,
+        partial,
+        cancel
+
+
+    }
+}
