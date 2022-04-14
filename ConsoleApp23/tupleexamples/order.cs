71437453803a71c9f80e6162959a7b24ec360e63
Md.Hasibul Hossain <mdhasibulhossain8@gmail.com>
2022-04-14 13:35:52 +0600

    adding

---
+++ ConsoleApp23/tupleexamples/order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tupleexamples
+{
+    public  class order
+    { public int id { get; set; }
+        
+        public int CustomerId { get; set; }
+
+        public double TotalAmount { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+
+        }
+    }
+}
